18d97551c3adef406e2e3b1bca149935524a1b17
Khalid Hasan <khalid.sr.hasan@gmail.com>
2025-02-03 00:46:58 +0600

    basic setup complte

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="text-center h-16 shadow-lg border">
+            <h2 className="leading-[4rem]">Header</h2>
+        </div>
+    );
+};
+
+export default Header;
